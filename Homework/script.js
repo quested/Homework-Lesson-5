879ccdaf23b90b22e013a14de6a32c7267a6a5d2
Edik Deineka <e.v.deineka@gmail.com>
2020-02-21 19:48:19 +0200

    first commit

---
+++ Homework/script.js
@@ -0,0 +1,72 @@
+//Задача 1
+var numbers = [1, 5, 99, 43, 101];
+var sum = 0;
+
+for (let num of numbers) {
+    sum += num;
+}
+console.log(sum);
+
+
+numbers.forEach(function(item) {
+    sum += item;
+}
+)
+console.log(sum);
+
+
+
+//Задача 2
+var newNumbers = [10, 50, 9, 4, 2];
+var result = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    result[i] = numbers[i] + newNumbers[i];
+}
+console.log(result);
+
+
+numbers.forEach(function(item, i) {
+    result[i] = item + newNumbers[i];
+}
+)
+console.log(result);
+
+
+
+// Задача 3
+var newSum = 0;
+
+numbers.forEach(function(item, i) {
+    newSum = newSum + item + numbers[i] + newNumbers[i];
+}
+)
+console.log(newSum);
+
+
+
+// Задача 4
+var newResult = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    newResult[i] = Math.pow(numbers[i], 2);
+}
+console.log(newResult);
+
+
+var newResult1 = numbers.map(function(item) {
+    return Math.pow(item, 2);
+}
+)
+console.log(newResult1);
+
+
+
+// Задача 5
+var names = ['Vlad', 'Max', 'Lena', 'Masha'];
+
+var newNames = names.filter(function(item) {
+    return item.length < 4;
+}
+)
+console.log(newNames);
